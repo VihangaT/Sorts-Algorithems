223508a7bcc6b522952b9ce62d7f1fb7af05652b
vihanga <liyanage.vihanga99@gmail.com>
2022-02-13 13:58:47 +0530

    implemented bubblesort

---
+++ src/sortClasses/BubbleSort.java
@@ -0,0 +1,44 @@
+package sortClasses;
+
+import java.util.Random;
+
+public class BubbleSort {
+
+
+    public static void main(String[] args){
+
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+
+        //BubbleSort
+        boolean swappedSomthing=true;
+        while(swappedSomthing) {
+            swappedSomthing=false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    swappedSomthing=true;
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i] = temp;
+                }
+            }
+        }
+
+        System.out.println("After:");
+        printArray(numbers);
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
